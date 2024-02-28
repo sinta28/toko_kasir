6bd04202ff8e253603c03f0da2c80b89c4019e67
sinta28 <161537909+sinta28@users.noreply.github.com>
2024-02-28 09:47:43 +0700

    Add files via upload

---
+++ db_kasir_ci4.sql
@@ -0,0 +1,303 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Feb 2024 pada 04.12
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kasir_ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_jual`
+--
+
+CREATE TABLE `tbl_jual` (
+  `id_jual` int(11) NOT NULL,
+  `no_faktur` varchar(15) DEFAULT NULL,
+  `tgl_jual` date DEFAULT NULL,
+  `jam` time DEFAULT NULL,
+  `grand_total` int(11) DEFAULT NULL,
+  `dibayar` int(11) DEFAULT NULL,
+  `kembalian` int(11) DEFAULT NULL,
+  `id_user` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_jual`
+--
+
+INSERT INTO `tbl_jual` (`id_jual`, `no_faktur`, `tgl_jual`, `jam`, `grand_total`, `dibayar`, `kembalian`, `id_user`) VALUES
+(7, '202402250001', '2024-02-25', '06:40:33', 75000, 150000, 75000, 1),
+(8, '202402250002', '2024-02-25', '06:58:54', 90000, 95000, 5000, 1),
+(9, '202402250003', '2024-02-25', '07:00:50', 100000, 100000, -90000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(2) NOT NULL,
+  `nama_kategori` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(2, 'Minuman'),
+(4, 'Elektronik'),
+(5, 'Alat Tulis Kantor'),
+(13, 'Makanan'),
+(14, 'cemilan'),
+(15, 'peralatan makan'),
+(16, 'peralatan mandi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(11) NOT NULL,
+  `kode_produk` varchar(25) DEFAULT NULL,
+  `nama_produk` varchar(150) DEFAULT NULL,
+  `id_kategori` int(2) DEFAULT NULL,
+  `id_satuan` int(2) DEFAULT NULL,
+  `harga_beli` int(11) DEFAULT NULL,
+  `harga_jual` int(11) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `kode_produk`, `nama_produk`, `id_kategori`, `id_satuan`, `harga_beli`, `harga_jual`, `stok`) VALUES
+(20, '1111', 'chocolatos', 16, 7, 8000, 10000, 0),
+(23, '222', 'mixue', 2, 4, 8000, 10000, 85),
+(24, '3333', 'pop ice', 2, 4, 5000, 10000, 500),
+(26, '555', 'Keripik Taro', 14, 5, 70000, 100000, 100),
+(27, '1122', 'chitatos', 14, 7, 10000, 20000, 100),
+(28, '444', 'sabun Giv', 16, 13, 7000, 12000, 500),
+(29, '777', 'sendok', 15, 3, 10000, 15000, 499),
+(30, '888', 'pulpen', 5, 4, 7000, 13000, 500),
+(31, '999', 'kacang garuda rosta', 14, 7, 2000, 4000, 500),
+(32, '1010', 'handuk', 16, 13, 20000, 30000, 500);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_rinci_jual`
+--
+
+CREATE TABLE `tbl_rinci_jual` (
+  `id_rinci` int(11) NOT NULL,
+  `no_faktur` varchar(15) DEFAULT NULL,
+  `kode_produk` varchar(25) DEFAULT NULL,
+  `modal` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL,
+  `untung` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_rinci_jual`
+--
+
+INSERT INTO `tbl_rinci_jual` (`id_rinci`, `no_faktur`, `kode_produk`, `modal`, `harga`, `qty`, `total_harga`, `untung`) VALUES
+(14, '202402250001', '1111', 8000, 10000, 1, 10000, 2000),
+(15, '202402250001', '777', 10000, 15000, 1, 15000, 5000),
+(16, '202402250001', '222', 8000, 10000, 5, 50000, 10000),
+(17, '202402250002', '1111', 8000, 10000, 9, 90000, 18000),
+(18, '202402250003', '1111', 8000, 10000, 10, 100000, 20000);
+
+--
+-- Trigger `tbl_rinci_jual`
+--
+DELIMITER $$
+CREATE TRIGGER `t_rinci_jual` AFTER INSERT ON `tbl_rinci_jual` FOR EACH ROW BEGIN
+	UPDATE tbl_produk SET stok = stok - NEW.qty
+    WHERE kode_produk = NEW.kode_produk;
+ END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id_satuan` int(2) NOT NULL,
+  `nama_satuan` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_satuan`
+--
+
+INSERT INTO `tbl_satuan` (`id_satuan`, `nama_satuan`) VALUES
+(2, 'Box '),
+(3, 'Lusin'),
+(4, 'pcs'),
+(5, 'KG'),
+(7, 'Bungkus'),
+(8, 'liter'),
+(13, 'satuan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_setting`
+--
+
+CREATE TABLE `tbl_setting` (
+  `id` int(1) NOT NULL,
+  `nama_toko` varchar(25) DEFAULT NULL,
+  `slogan` varchar(50) DEFAULT NULL,
+  `alamat` varchar(200) DEFAULT NULL,
+  `no_telpon` varchar(18) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_setting`
+--
+
+INSERT INTO `tbl_setting` (`id`, `nama_toko`, `slogan`, `alamat`, `no_telpon`, `deskripsi`) VALUES
+(1, 'Cahyati Store', 'Toko Serba Ada Menyediakan Segalanya', 'Jl.Patimura No.27 kec.Sindang Agung, Kab.Kuningan', '083195794036', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(2) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`) VALUES
+(1, 'fika cahyati', 'admin@gmail.com', '123', 1),
+(2, 'fika', 'kasir@gmail.com', '123', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  ADD PRIMARY KEY (`id_jual`);
+
+--
+-- Indeks untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indeks untuk tabel `tbl_rinci_jual`
+--
+ALTER TABLE `tbl_rinci_jual`
+  ADD PRIMARY KEY (`id_rinci`);
+
+--
+-- Indeks untuk tabel `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indeks untuk tabel `tbl_setting`
+--
+ALTER TABLE `tbl_setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_jual`
+--
+ALTER TABLE `tbl_jual`
+  MODIFY `id_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_rinci_jual`
+--
+ALTER TABLE `tbl_rinci_jual`
+  MODIFY `id_rinci` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id_satuan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
